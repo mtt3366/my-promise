0ad5a4ff6ce08aaa53c3453b972630636afd0770
马涛涛 <1030557028@qq.com>
2021-06-24 15:59:19 +0800

    throw抛出错误改变状态

--- promise.js
+++ promise.js
@@ -19,9 +19,13 @@ function Promise(executor){
         //2. 设置对象结果值 (promiseResult)
         self.PromiseResult = data;
     }
-
-    //同步调用『执行器函数』
-    executor(resolve, reject);
+    try{
+        //同步调用『执行器函数』
+        executor(resolve, reject);
+    }catch(e){
+        //修改 promise 对象状态为『失败』
+        reject(e);
+    }
 }
 
 //添加 then 方法
