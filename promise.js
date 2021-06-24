c5f661930b815afd1f2e9810022da81b1187ce16
马涛涛 <1030557028@qq.com>
2021-06-24 16:04:29 +0800

    添加几个静态方法

--- promise.js
+++ promise.js
@@ -111,3 +111,70 @@ Promise.prototype.then = function(onResolved, onRejected){
 Promise.prototype.catch = function(onRejected){
     return this.then(undefined, onRejected);
 }
+
+//添加 resolve 方法
+Promise.resolve = function(value){
+    //返回promise对象
+    return new Promise((resolve, reject) => {
+        if(value instanceof Promise){
+            value.then(v=>{
+                resolve(v);
+            }, r=>{
+                reject(r);
+            })
+        }else{
+            //状态设置为成功
+            resolve(value);
+        }
+    });
+}
+
+//添加 reject 方法
+Promise.reject = function(reason){
+    return new Promise((resolve, reject)=>{
+        reject(reason);
+    });
+}
+
+//添加 all 方法
+Promise.all = function(promises){
+    //返回结果为promise对象
+    return new Promise((resolve, reject) => {
+        //声明变量
+        let count = 0;
+        let arr = [];
+        //遍历
+        for(let i=0;i<promises.length;i++){
+            //
+            promises[i].then(v => {
+                //得知对象的状态是成功
+                //每个promise对象 都成功
+                count++;
+                //将当前promise对象成功的结果 存入到数组中
+                arr[i] = v;
+                //判断
+                if(count === promises.length){
+                    //修改状态
+                    resolve(arr);
+                }
+            }, r => {
+                reject(r);
+            });
+        }
+    });
+}
+
+//添加 race 方法
+Promise.race = function(promises){
+    return new Promise((resolve, reject) => {
+        for(let i=0;i<promises.length;i++){
+            promises[i].then(v => {
+                //修改返回对象的状态为 『成功』
+                resolve(v);
+            },r=>{
+                //修改返回对象的状态为 『失败』
+                reject(r);
+            })
+        }
+    });
+}
